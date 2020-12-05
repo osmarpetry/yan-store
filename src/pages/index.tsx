e3f5da3164e3196c4d1c6d56488a59082033cb20
Osmar Petry <osmarpetry@gmail.com>
2020-12-05 17:44:49 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import FullpageInformation from 'components/FullpageInformation'
+
+export default function Home() {
+  return (
+    <>
+      <FullpageInformation
+        title="Webside in works"
+        description="Come back later!"
+      />
+    </>
+  )
+}
